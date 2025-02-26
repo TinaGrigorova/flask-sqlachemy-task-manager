e180399602fec610df4f588f1a3d701b3e9e6b5a
Tina Grigorova <tina.grigorova@students.codeinstitute.net>
2025-02-26 16:32:34 +0000

    migrate teh db

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -7,7 +7,7 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://neonDB_owner:YOUR_PASSWORD@ep-hidden-leaf-a25em0f.eu-central-1.aws.neon.tech/mop_alias_thorn_845685?sslmode=require"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://neondb_owner:npg_kKCG63pOwxvX@ep-hidden-leaf-a25emr0f.eu-central-1.aws.neon.tech/mop_alias_thorn_845685"
 
 
 db = SQLAlchemy(app)
