3b74cecec05de56b71aef4ef4186256e01c487a8
Tina Grigorova <tina.grigorova@students.codeinstitute.net>
2025-02-26 17:02:36 +0000

    add route in routes for category - edit_category

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -22,3 +22,7 @@ def add_category():
         db.session.commit()
         return redirect(url_for("categories"))
     return render_template("add_category.html")
+
+@app.route("/edit_category", methods=["GET", "POST"])
+def edit_catergory():
+    return render_template(edit_catergory.html)
